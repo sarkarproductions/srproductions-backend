1a04709d41c390791538258f2d0287bde8708c07
techiemanish <manishtiwari627@gmail.com>
2022-05-08 01:02:30 +0530

    Frontend integration for CORS in Admin

--- src/main/java/com/srproductions/srproductions/Admin/controller/AdminController.java
+++ src/main/java/com/srproductions/srproductions/Admin/controller/AdminController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000/"})
+@CrossOrigin(origins = {"https://sarkaragency.netlify.app/"})
 public class AdminController {
 
     @Autowired
